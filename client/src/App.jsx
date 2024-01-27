e6b958a9f6131f820dcd5a514d6b26374c2b8db2
Avi Mishra <avimishra261@gmail.com>
2024-01-27 13:27:23 +0530

    created pages for webapp

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,24 @@
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Dashboard from './pages/Dashboard'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Projects from './pages/Projects'
+import About from './pages/About'
 
 function App() {
 
   return (
-    <>
-    <h1>Hello World!</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-out" element={<SignUp />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 

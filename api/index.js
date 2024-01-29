02a603933473be1d16104d1dcb3f85926de24e90
Avi Mishra <avimishra261@gmail.com>
2024-01-29 14:33:23 +0530

    created middleware to handle error

--- api/index.js
+++ api/index.js
@@ -23,3 +23,15 @@ app.listen(3000, () => {
 
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
+
+// Middleware
+
+app.use((err,req,res,next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    res.status(statusCode).json({
+        successs:false,
+        statusCode,
+        message
+    });
+});
